4762d2c6affb8885f12eb17d601d5de193b92d91
Ian Reah <ian.reah@gmail.com>
2012-11-05 13:03:24 +0000

    Added unit test project

---
+++ MockStock.Tests/SubscriptionManagerTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MockStock.Tests
+{
+	[TestClass]
+	public class SubscriptionManagerTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			
+		}
+	}
+}
