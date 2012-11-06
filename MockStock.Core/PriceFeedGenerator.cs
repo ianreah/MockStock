b54006f0bd258da5f93d7709437059ef45cfebfe
Ian Reah <ian.reah@gmail.com>
2012-11-06 13:00:17 +0000

    Bugfix: All price feeds were for 'XYZ' symbol!

--- MockStock.Core/PriceFeedGenerator.cs
+++ MockStock.Core/PriceFeedGenerator.cs
@@ -15,7 +15,7 @@ namespace MockStock.Core
 		public IDisposable Generate(string symbol)
 		{
 			var random = new Random();
-			var observable = Observable.Generate(new StockPrice("XYZ", random.NextDouble() * 100), // starting value random price 0 - 100
+			var observable = Observable.Generate(new StockPrice(symbol, random.NextDouble() * 100), // starting value random price 0 - 100
 			                                     p => true,                                        // condition - keep going forever!
 			                                     p => p.NextPrice(random.NextDouble() - 0.5),      // update the price with a random fluctuation between -0.5 and +0.5
 			                                     p => p,                                           // selector - just return the price object
